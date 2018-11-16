67b3578eadb12453dda9a4713abaa8bcd71fbee3
Alex "Splitz" Davis <32062643+Splitzy@users.noreply.github.com>
2018-11-16 13:41:48 +0000

    Fixed Respawn issues
    Fixed an issue with the respawn where the velocity of the car would carry over. Also added a respawn button with the R button.

--- Block Assault/Assets/_Scripts/Checkpoint.cs
+++ Block Assault/Assets/_Scripts/Checkpoint.cs
@@ -5,21 +5,37 @@ using UnityEngine;
 public class Checkpoint : MonoBehaviour {
     public Transform checkpoint;
     GameObject player;
+    public Rigidbody rb;
 
 	// Use this for initialization
 	void Start()
     {
         player = GameObject.FindWithTag("Player");
 	}
-	
-	// Update is called once per frame
-	void OnTriggerEnter (Collider other)
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            Reset();
+        }
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter (Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
-            player.transform.position = checkpoint.position;
-            player.transform.rotation = checkpoint.rotation;
+            Reset();
         }
+    }
+
+    void Reset()
+    {
+        player.transform.position = checkpoint.position;
+        player.transform.rotation = checkpoint.rotation;
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
     }
 
 }
